93c8950c9e619ac36e61480190a0581e2503d632
Alexander Fokin <alexander.fokin@genesys.com>
2016-08-08 23:34:50 +0400

    redmine + docker

--- docker/run.sh
+++ docker/run.sh
@@ -23,8 +23,8 @@ bundle install --without development test rmagick
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
 touch /home/redmine/bootstrapped
-R_KEY=`bundle exec rake secret 2> /dev/null`
-echo -e "production:\nsecret_key_base: $R_KEY" > config/secrets.yml
+#R_KEY=`bundle exec rake secret 2> /dev/null`
+#echo -e "production:\nsecret_key_base: $R_KEY" > config/secrets.yml
 RAILS_ENV=production rails s -b 0.0.0.0
 EOF
 
@@ -33,6 +33,7 @@ else
 service mysql start && \
 su - redmine <<'EOF'
 cd /home/redmine/src
+bundle exec rake generate_secret_token
 RAILS_ENV=production rails s -b 0.0.0.0
 EOF
 
