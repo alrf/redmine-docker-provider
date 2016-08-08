76875373938acc1f8f5560333c686f1ab20e6c80
Alexander Fokin <alexander.fokin@genesys.com>
2016-08-08 16:31:39 +0400

    run.sh

--- run.sh
+++ run.sh
@@ -19,6 +19,7 @@ service mysql start && mysql -u root --password=root -e "CREATE USER '$R_USER'@'
 su - redmine <<'EOF'
 cd /home/redmine/src
 bundle exec rake generate_secret_token
+echo -e "production:\nsecret_key_base: `rake secret 2> /dev/null`" > config/secrets.yml
 bundle install --without development test rmagick
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
