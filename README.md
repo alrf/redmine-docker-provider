478ae4f3d4a1cc163a801f3b99f7412393c23a51
Alexander Fokin <alexander.fokin@genesys.com>
2016-08-09 19:27:18 +0400

    redmine + docker

--- README.md
+++ README.md
@@ -4,21 +4,3 @@ Redmine docker image based on alrf/ubuntu-rvm-rails docker image (https://github
 Build image as:  
 clone this repo  
 run `vagrant up`  
-
-Run as:  
-docker run -dit -v /home/docker_provider/redmine:/home/redmine/src -p 3000:3000 f41146a3e7a3  
-where:  
-/home/docker_provider/redmine - host-based directory, which contains the Redmine sources.  
-Don't forget add the config/database.yml file.  
-MySQL user and database for Redmine will be created automatically based on config/database.yml file.  
-/home/redmine/src - container directory, which will be contain the Redmine sources from host.  
-f41146a3e7a3 - image id  (builded on vagrant up)  
-
-Step by step:  
-cd /home/user1  
-git clone https://github.com/alrf/redmine-docker-provider.git  
-cd /home/user1/redmine-docker-provider  
-git clone https://github.com/alrf/redmine.git  
-Add the config/database.yml file into /home/user1/redmine-docker-provider/redmine/config directory  
-vagrant up  
-docker run -dit -v /home/user1/redmine-docker-provider/redmine:/home/redmine/src -p 3000:3000 \<image_id\>  
--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -8,14 +8,6 @@ chown -R redmine.redmine /home/redmine/src
 if [ ! -f /home/redmine/bootstrapped ]
 then
 
-#CFILE="/home/redmine/src/config/database.yml"
-#R_DB=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep database | cut -d':' -f2 | xargs`
-#R_HOST=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep host | cut -d':' -f2 | xargs`
-#R_USER=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep username | cut -d':' -f2 | xargs`
-#R_PW=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep password | cut -d':' -f2 | xargs`
-
-#service mysql start && mysql -u root --password=root -e "CREATE USER '$R_USER'@'$R_HOST' IDENTIFIED BY '$R_PW'; CREATE DATABASE IF NOT EXISTS $R_DB; GRANT ALL ON $R_DB.* TO '$R_USER'@'$R_HOST'; flush privileges;"
-
 su - redmine <<'EOF'
 cd /home/redmine/src
 bundle exec rake generate_secret_token
@@ -23,14 +15,11 @@ bundle install --without development test rmagick
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
 touch /home/redmine/bootstrapped
-#R_KEY=`bundle exec rake secret 2> /dev/null`
-#echo -e "production:\nsecret_key_base: $R_KEY" > config/secrets.yml
 RAILS_ENV=production rails s -b 0.0.0.0
 EOF
 
 else
 
-#service mysql start && \
 su - redmine <<'EOF'
 cd /home/redmine/src
 bundle exec rake generate_secret_token
