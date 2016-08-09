899c4ca776624d543c3a891656c97e00c411f06a
Alexander Fokin <alexander.fokin@genesys.com>
2016-08-09 14:46:58 +0400

    redmine + docker

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -8,13 +8,13 @@ chown -R redmine.redmine /home/redmine/src
 if [ ! -f /home/redmine/bootstrapped ]
 then
 
-CFILE="/home/redmine/src/config/database.yml"
-R_DB=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep database | cut -d':' -f2 | xargs`
-R_HOST=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep host | cut -d':' -f2 | xargs`
-R_USER=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep username | cut -d':' -f2 | xargs`
-R_PW=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep password | cut -d':' -f2 | xargs`
+#CFILE="/home/redmine/src/config/database.yml"
+#R_DB=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep database | cut -d':' -f2 | xargs`
+#R_HOST=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep host | cut -d':' -f2 | xargs`
+#R_USER=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep username | cut -d':' -f2 | xargs`
+#R_PW=`grep 'production:' -A 5 $CFILE | grep -E -v '#|--|^$' | grep password | cut -d':' -f2 | xargs`
 
-service mysql start && mysql -u root --password=root -e "CREATE USER '$R_USER'@'$R_HOST' IDENTIFIED BY '$R_PW'; CREATE DATABASE IF NOT EXISTS $R_DB; GRANT ALL ON $R_DB.* TO '$R_USER'@'$R_HOST'; flush privileges;"
+#service mysql start && mysql -u root --password=root -e "CREATE USER '$R_USER'@'$R_HOST' IDENTIFIED BY '$R_PW'; CREATE DATABASE IF NOT EXISTS $R_DB; GRANT ALL ON $R_DB.* TO '$R_USER'@'$R_HOST'; flush privileges;"
 
 su - redmine <<'EOF'
 cd /home/redmine/src
@@ -30,7 +30,7 @@ EOF
 
 else
 
-service mysql start && \
+#service mysql start && \
 su - redmine <<'EOF'
 cd /home/redmine/src
 bundle exec rake generate_secret_token
