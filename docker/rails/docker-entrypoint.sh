655fd41b60b6694ef7afb36b4f3ecba8abb11fb8
Alexander Fokin <alexander.fokin@genesys.com>
2016-08-17 16:52:38 +0400

    docker-entrypoint.sh

--- docker/rails/docker-entrypoint.sh
+++ docker/rails/docker-entrypoint.sh
@@ -10,20 +10,27 @@ then
 
 su - redmine <<'EOF'
 cd /home/redmine/src
-bundle exec rake generate_secret_token
 bundle install --without development test rmagick
 RAILS_ENV=production bundle exec rake db:migrate
 RAILS_ENV=production REDMINE_LANG=en bundle exec rake redmine:load_default_data
 touch /home/redmine/bootstrapped
-RAILS_ENV=production rails s -b 0.0.0.0
 EOF
 
-else
+fi
+
+
+if [ ! -f /home/redmine/src/config/initializers/secret_token.rb ]
+then
 
 su - redmine <<'EOF'
 cd /home/redmine/src
 bundle exec rake generate_secret_token
-RAILS_ENV=production rails s -b 0.0.0.0
 EOF
 
 fi
+
+
+su - redmine <<'EOF'
+cd /home/redmine/src
+RAILS_ENV=production rails s -b 0.0.0.0
+EOF
